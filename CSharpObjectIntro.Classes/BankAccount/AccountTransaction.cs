66b3ce564e43ae88a40221058a63056cfa94954d
ian <AverreI@StPaulsSchool.org.uk>
2022-10-07 11:59:50 +0100

    done x2

---
+++ CSharpObjectIntro.Classes/BankAccount/AccountTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpObjectIntro.Classes.BankAccount
+{
+    public class AccountTransaction
+    {
+        public AccountTransaction(DateTime date, double amount, double newBalance, string category, string counterparty, string transactionType, string description)
+        {
+            Date = date;
+            Amount = amount;
+            Category = category;
+            Counterparty = counterparty;
+            TransactionType = transactionType;
+            Description = description;
+            NewBalance = newBalance;
+        }
+        public DateTime Date { get; private set; }
+        public double Amount { get; private set; }
+        public string Category { get; private set; }
+        public string Counterparty { get; private set; }
+        public string TransactionType { get; private set; }
+        public string Description { get; private set; }
+        public double NewBalance { get; private set; }
+    }
+}
+
---
+++ CSharpObjectIntro.Tests/BankAccountTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpObjectIntro.Classes.BankAccount;
+
+namespace CSharpObjectIntro.Tests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestBankAccount()
+        {
+            var BankAccount = new BankAccount(0);
+            Assert.AreEqual(0, BankAccount.Balance);
+        }
+    }
+}
